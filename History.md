770804c6c87684be2d2a099d17d43244c67f613a
Mike Perham <mperham@gmail.com>
2011-03-23 20:03:26 -0700

    Add change history

---
+++ History.md
@@ -0,0 +1,5 @@
+Changes
+================
+
+0.1.1
+ - Add 'list', an alias for 'ls' (jnewland)
--- lib/lunchy.rb
+++ lib/lunchy.rb
@@ -1,5 +1,5 @@
 class Lunchy
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   def start(params)
     raise ArgumentError, "start [name]" if params.empty?
